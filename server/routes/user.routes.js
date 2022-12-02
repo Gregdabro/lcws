9a128fa2cd11424ee50f853157ac57f3fc916af4
Gregdabro <gregmonak@gmail.com>
2022-12-02 12:38:54 +0100

    added user controller

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,6 +2,9 @@ const express = require("express")
 const router = express.Router({ mergeParams: true })
 const userController = require("../controllers/userController")
 
+router.get("/registration", userController.registration)
+router.get("/login", userController.login)
+
 router.get("/auth", userController.check)
 
 module.exports = router
